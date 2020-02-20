da341b51402d873233ec6aac1d232fb354ec50f5
Erika Boomker-Lategan <erika.boomker-lategan@impact.com>
2020-02-20 13:10:33 +0200

    refactoring to common crud interface. Closes #10.

--- erikapetclinic-data/src/main/java/org/erika/erikapetclinic/services/OwnerService.java
+++ erikapetclinic-data/src/main/java/org/erika/erikapetclinic/services/OwnerService.java
@@ -2,17 +2,8 @@ package org.erika.erikapetclinic.services;
 
 import org.erika.erikapetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLatName (String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 
 }
